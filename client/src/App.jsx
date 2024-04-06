cbe691d5f82ee8975ad5bbcb16930f2208d721dc
SiriChandhana06 <sirisiri3006@gmail.com>
2024-04-06 19:00:40 +0530

    react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Home from './Pages/Home';
 import About from './Pages/About';
 import Footer from './Components/Footer';
 import Coe from './Pages/Coe';
-
+import Vov from './Pages/Vov';
 
 
 export default function App() {
@@ -19,7 +19,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About/>} />
-          <Route path="coe" element={<Coe />} />
+          <Route path="/coe" element={<Coe />} />
+          <Route path="/vov" element={<Vov />} />
         </Routes>
         <Footer/>
     </BrowserRouter>
--- client/src/Pages/Vov.jsx
+++ client/src/Pages/Vov.jsx
@@ -18,7 +18,7 @@ function Vov(){
                 <div className='m-4 pt-10 sm:m-6 md:m-10'>
                     <div className="border-2 border-solid border-gray-200 shadow-2xl shadow-gray-600">
                         <div>
-                            <h1 className='p-4 text-xl font-semibold md:p-10 text-2xl'>Chairman's Message</h1>
+                            <h1 className='p-4 text-xl font-semibold md:p-10 md:text-2xl'>Chairman's Message</h1>
                         </div>
                         <div className='md:flex'>
                             <div className='flex justify-center md:px-16 py-6 h-auto w-full'>
@@ -27,7 +27,7 @@ function Vov(){
                             <div className='p-4'>
                                 <h1 className='text-md md:font-medium'>Delightful task! to rear the tender thought;<br/>To teach the young how to shoot;<br/>To pour the fresh, instruction over the mind ;<br/>To breathe the enlivening spirit, and<br/>To fix the generous purpose in the glowing heart.</h1>
                                 <p  className=' pt-2 text-blue-500 font-medium md:font-semibold text-lg'>“Count your chick before it hatches” </p>
-                                <p className='text-justify md:font-medium'>says an old Adage. Our JAYA ENGINEERING COLLEGE is gearing-up such skills to the budding Engineers. In this wide arena of competition, we would like to enable Engineering aspirants with more calibers and more innovation.</p>
+                                <p className='text-justify md:font-medium pr-6'>says an old Adage. Our JAYA ENGINEERING COLLEGE is gearing-up such skills to the budding Engineers. In this wide arena of competition, we would like to enable Engineering aspirants with more calibers and more innovation.</p>
                                 <p className='font-medium pl-32 pt-4 md:pl-48 md:text-lg lg:pl-80 lg:text-xl'>-Prof.A. Kanagaraj M.A., M.Phil.</p>
                                 <p className='text-blue-500 font-medium pl-48 md:pl-72 md:font-semibold md:text-lg lg:pl-96 lg:ml-6 lg:text-xl'>Chairman</p>
                             </div>
@@ -41,8 +41,8 @@ function Vov(){
                         </div>
                         <div className='md:flex'>
                             <div className='py-4'>
-                                <p className='text-justify px-4 pt-2 md:font-medium'>Dr. K.L. Shunmuganathan is an illustrious Professor in the Department of Computer Science and Engineering, Jaya Group of Institutions. He has a wide experience in Teaching, Research and Administration for more than 31 years. He earned his Ph.D Degree under the Faculty of Information and Communication Engineering, Anna University, Chennai in the area of AI and DBMS. He holds Masters Degree in Computer Science Engineering in Arulmigu Kalasalingam College of Engineering, Krishnan koil in the year 1998 and Bachelor  degree in Computer Science Engineering from Bharathidasan University, Trichy.</p>
-                                <p className='text-justify px-4 pt-2 md:font-medium'>He started his teaching career from a reputed Technical Institution as a Lecturer in the year 1991. Then, he served as a Lecturer (Senior Grade), Assistant Professor and Professor & Head in a reputed Engineering College from 1991-2005. He has been working in Jaya Group of Institutions since 2022.  He is recognized supervisor in Anna University, Faculty of Information & communication Engineering. He has produced 30 Ph.Ds and published 71 Scopus journals, web of science 65 journals and SCI 11 journals. He has held various administrative positions in Engineering colleges including Deputy Director, Principal. He was instrumental in bringing Outcome Based Education.</p>
+                                <p className='text-justify px-4 pt-2 md:font-medium px-6'>Dr. K.L. Shunmuganathan is an illustrious Professor in the Department of Computer Science and Engineering, Jaya Group of Institutions. He has a wide experience in Teaching, Research and Administration for more than 31 years. He earned his Ph.D Degree under the Faculty of Information and Communication Engineering, Anna University, Chennai in the area of AI and DBMS. He holds Masters Degree in Computer Science Engineering in Arulmigu Kalasalingam College of Engineering, Krishnan koil in the year 1998 and Bachelor  degree in Computer Science Engineering from Bharathidasan University, Trichy.</p>
+                                <p className='text-justify px-4 pt-2 md:font-medium px-6'>He started his teaching career from a reputed Technical Institution as a Lecturer in the year 1991. Then, he served as a Lecturer (Senior Grade), Assistant Professor and Professor & Head in a reputed Engineering College from 1991-2005. He has been working in Jaya Group of Institutions since 2022.  He is recognized supervisor in Anna University, Faculty of Information & communication Engineering. He has produced 30 Ph.Ds and published 71 Scopus journals, web of science 65 journals and SCI 11 journals. He has held various administrative positions in Engineering colleges including Deputy Director, Principal. He was instrumental in bringing Outcome Based Education.</p>
                                 <p className='font-medium pl-28 pt-4 md:pl-20 text-lg lg:pl-80 text-xl'>-Dr. K.L. Shunmuganathan, B.E(CSE).,M.S.(SS).,M.E(CSE).,PhD.(CSE),FIE</p>
                                 <p className='text-blue-500 font-medium pl-48 md:pl-80 md:font-semibold text-lg lg:pl-96 ml-48 text-xl'>Principal</p>
                             </div>
@@ -55,15 +55,15 @@ function Vov(){
                 <div className='m-4 pt-10 sm:m-6 md:m-10'>
                     <div className="border-2 border-solid border-gray-200 shadow-2xl shadow-gray-600">
                         <div>
-                            <h1 className='p-4 text-xl font-semibold md:p-10 text-2xl'>Vice chairman's Message</h1>
+                            <h1 className='p-4 text-xl font-semibold md:p-10 md:text-2xl'>Vice chairman's Message</h1>
                         </div>
                         <div className='md:flex'>
                             <div className='flex justify-center md:px-10 py-6 h-auto w-full'>
                                 <img src={vc} className=''/>
                             </div>
                             <div className='py-4'>
-                                <p className=' text-justify px-4 pt-2 md:font-medium'>“Mind has thousand eyes But the Heart with one yet the light of whole life done when the Education is in” by adhering these words our Jaya Engineering College the Educational Tree has it’s own branch and extending great shelter to Enthusiastic Engineering Candidates.</p>
-                                <p className='text-justify px-4 pt-2 md:font-medium'>It has great Milestone of Achievement in the ventures on various fields like Aeronautic, Computer Science, Information Technology, Electrical and Electronic, Electronic and Communication, Civil, Textile and Mechanical Engineering. We are paying at most interest over the standard of Education by the overseas experiences. Thereby we are building the castle of career set-up to the upcoming Engineers.</p>
+                                <p className=' text-justify px-4 pt-2 md:font-medium pr-6'>“Mind has thousand eyes But the Heart with one yet the light of whole life done when the Education is in” by adhering these words our Jaya Engineering College the Educational Tree has it’s own branch and extending great shelter to Enthusiastic Engineering Candidates.</p>
+                                <p className='text-justify px-4 pt-2 md:font-medium pr-6'>It has great Milestone of Achievement in the ventures on various fields like Aeronautic, Computer Science, Information Technology, Electrical and Electronic, Electronic and Communication, Civil, Textile and Mechanical Engineering. We are paying at most interest over the standard of Education by the overseas experiences. Thereby we are building the castle of career set-up to the upcoming Engineers.</p>
                                 <p className='font-medium pl-28 pt-4 md:pl-64 text-lg lg:pl-96 ml-28 text-xl'>-Er. K.Navaraj, M.Tech., (Ph.D.)</p>
                                 <p className='text-blue-500 font-medium pl-44 md:pl-80 font-semibold text-lg lg:pl-96 ml-44 text-xl'>Vice-Chairman</p>
                             </div>
@@ -73,13 +73,13 @@ function Vov(){
                 <div className='m-4 pt-10 sm:m-6 md:m-10'>
                     <div className="border-2 border-solid border-gray-200 shadow-2xl shadow-gray-600">
                         <div>
-                            <h1 className='p-4 text-xl font-semibold md:p-10 text-2xl'>Dean-Research Message</h1>
+                            <h1 className='p-4 text-xl font-semibold md:p-10 md:text-2xl'>Dean-Research Message</h1>
                         </div>
                         <div className='md:flex'>
                             <div className='py-4'>
-                                <p className=' text-justify px-4 pt-2 md:font-medium'>I take pleasure in welcoming you to the website of the institution, which is a part of the Jaya Group of Institutions with 46+ years of excellence. The Group encompasses almost all academic disciplines – Medicine & Surgery, Engineering, Technology, Business Administration, Commerce, Dentistry, Paramedic, Life Sciences, Pedagogy (Teacher Education), Hospitality, Media, Culture, etc.</p>
-                                <p className=' text-justify px-4 pt-2 md:font-medium'>Research & Development activities help the students to develop technical skills as well as the soft skills in Innovation and Entrepreneurship. Such skills are very much required for students in any future career they pursue – in the industry, academics or entrepreneurship.</p>
-                                <p className=' text-justify px-4 pt-2 md:font-medium'>Research & Development activities include Research paper publications, Innovation Contests, establishing startup companies, acquiring Intellectual Property Rights (IPR), (including patents), collaborative research / product development with industry, activities in Centers of Excellence (CoE), research projects with external funding and others.</p>
+                                <p className=' text-justify px-4 pt-2 md:font-medium px-6'>I take pleasure in welcoming you to the website of the institution, which is a part of the Jaya Group of Institutions with 46+ years of excellence. The Group encompasses almost all academic disciplines – Medicine & Surgery, Engineering, Technology, Business Administration, Commerce, Dentistry, Paramedic, Life Sciences, Pedagogy (Teacher Education), Hospitality, Media, Culture, etc.</p>
+                                <p className=' text-justify px-4 pt-2 md:font-medium px-6'>Research & Development activities help the students to develop technical skills as well as the soft skills in Innovation and Entrepreneurship. Such skills are very much required for students in any future career they pursue – in the industry, academics or entrepreneurship.</p>
+                                <p className=' text-justify px-4 pt-2 md:font-medium px-6'>Research & Development activities include Research paper publications, Innovation Contests, establishing startup companies, acquiring Intellectual Property Rights (IPR), (including patents), collaborative research / product development with industry, activities in Centers of Excellence (CoE), research projects with external funding and others.</p>
                                 <p className='font-medium pl-32 pt-4 md:pl-80 text-lg lg:pl-96 ml-16 text-xl'>-Dr.M.SOMASUNDARAM</p>
                                 <p className='text-blue-500 text-center font-medium pl-20 md:pl-72 font-semibold text-lg lg:text-xl '>DEAN – RESEARCH, JAYA GROUP OF INSTITUTIONS</p>
                             </div>
@@ -92,15 +92,15 @@ function Vov(){
                 <div className='m-4 pt-10 sm:m-6 md:m-10'>
                     <div className="border-2 border-solid border-gray-200 shadow-2xl shadow-gray-600">
                         <div>
-                            <h1 className='p-4 text-xl font-semibold md:p-10 text-2xl'>Vice Principal's Message</h1>
+                            <h1 className='p-4 text-xl font-semibold md:p-10 md:text-2xl'>Vice Principal's Message</h1>
                         </div>
                         <div className='md:flex'>
                             <div className='flex justify-center md:px-10 py-6 h-auto w-full'>
                                 <img src={vp} className=''/>
                             </div>
                             <div className='py-4'>
-                                <p className=' text-justify px-4 pt-2 md:font-medium'>The Vice Principal of the college obtained B.E. degree in Industrial Engineering in faculty of Mechanical Engineering from College of Engineering Guindy Anna University Chennai. He finished his Masters in Engineering Design at Anjalai Ammal – Mahalingam Engineering College, Thiruvarur, and Tiruchirappalli. He was awarded Ph.D. degree in the year 2016 by the faculty of Mechanical Engineering, Anna University, and Chennai.</p>
-                                <p className=' text-justify px-4 pt-2 md:font-medium'>He has 11 years of academic experience. He has an expertise in the field of flow analysis in IC engine and Engineering Design. He has published 12 papers in reputed International Journals, 5 in International Conferences and 2 in National Conferences. He has guided more than 30 UG projects and 10 PG Research projects.</p>
+                                <p className=' text-justify px-4 pt-2 md:font-medium pr-6'>The Vice Principal of the college obtained B.E. degree in Industrial Engineering in faculty of Mechanical Engineering from College of Engineering Guindy Anna University Chennai. He finished his Masters in Engineering Design at Anjalai Ammal – Mahalingam Engineering College, Thiruvarur, and Tiruchirappalli. He was awarded Ph.D. degree in the year 2016 by the faculty of Mechanical Engineering, Anna University, and Chennai.</p>
+                                <p className=' text-justify px-4 pt-2 md:font-medium pr-6'>He has 11 years of academic experience. He has an expertise in the field of flow analysis in IC engine and Engineering Design. He has published 12 papers in reputed International Journals, 5 in International Conferences and 2 in National Conferences. He has guided more than 30 UG projects and 10 PG Research projects.</p>
                                 <p className='font-medium pl-28 pt-4 md:pl-64 text-lg lg:pl-96 ml-24 text-xl'>-Dr. S. Rajendran, M.E., Ph.D</p>
                                 <p className='text-blue-500 font-medium pl-44 md:pl-80 font-semibold text-lg lg:pl-96 ml-40 text-xl '>Vice-Principal</p>
                             </div>
@@ -110,15 +110,15 @@ function Vov(){
                 <div className='m-4 pt-10 sm:m-6 md:m-10'>
                     <div className="border-2 border-solid border-gray-200 shadow-2xl shadow-gray-600">
                         <div>
-                            <h1 className='p-4 text-xl font-semibold md:p-10 text-2xl'>Secretary Message</h1>
+                            <h1 className='p-4 text-xl font-semibold md:p-10 md:text-2xl'>Secretary Message</h1>
                         </div>
                         <div className='md:flex'>
                             <div className='py-4'>
-                                <p className='pl-4 pt-2 text-blue-500 font-medium md:font-semibold text-lg'> To strive; to seek & to achieve; This is the age </p>
-                                <p className='text-justify px-4 pt-2 md:font-medium'>We are launching in all hearts of forth-coming Engineering doers. The vision of worldwide corporate scope has been given and still continues in our reputed Jaya Engineering College. We are giving helping-aid to various technical innovations and stimulating those projects for worldwide knowledge.</p>
-                                <p className='text-justify px-4 pt-2 md:font-medium'> Like a Mother assuaging the pain to success of child, we are indeed find immense pleasure in helping economically lagging but mentally capable students to obtain scholarships, sponsors, bank loans and other endowment prizes. Like a well known proverb, this is not an end; but a beginning. Our Jaya Engineering College will do Educational Service in Eternal Manner.</p>
-                                <p className='font-medium pl-28 pt-4 md:pl-60 text-lg lg:pl-96 ml-10 text-2xl'>-Smt. K.Vijaya Kumari, M.A., B.Ed.</p>
-                                <p className='text-blue-500 font-medium pl-48 md:pl-80 font-semibold text-lg lg:pl-96 ml-40 text-xl'>Secretary</p>
+                                <p className='pl-4 pt-2 text-blue-500 font-medium md:font-semibold text-lg pl-6'> To strive; to seek & to achieve; This is the age </p>
+                                <p className='text-justify px-4 pt-2 md:font-medium px-6'>We are launching in all hearts of forth-coming Engineering doers. The vision of worldwide corporate scope has been given and still continues in our reputed Jaya Engineering College. We are giving helping-aid to various technical innovations and stimulating those projects for worldwide knowledge.</p>
+                                <p className='text-justify px-4 pt-2 md:font-medium px-6'> Like a Mother assuaging the pain to success of child, we are indeed find immense pleasure in helping economically lagging but mentally capable students to obtain scholarships, sponsors, bank loans and other endowment prizes. Like a well known proverb, this is not an end; but a beginning. Our Jaya Engineering College will do Educational Service in Eternal Manner.</p>
+                                <p className='font-medium pl-28 pt-4 md:pl-60 text-lg lg:pl-96 ml-16 lg:text-2xl'>-Smt. K.Vijaya Kumari, M.A., B.Ed.</p>
+                                <p className='text-blue-500 font-medium pl-48 md:pl-80 font-semibold text-lg lg:pl-96 ml-48 text-xl'>Secretary</p>
                             </div>
                             <div className='flex justify-center md:px-10 py-6 h-auto w-full'>
                                 <img src={sm} className=''/>
